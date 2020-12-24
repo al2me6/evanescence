735b9ef4d1a8624011776a14038b95b2783b038b
github-action-benchmark <github@users.noreply.github.com>
2020-12-24 04:34:42 +0000

    add evanescence_core benchmarks (cargo) benchmark result for f480aa876d79d019d1ff38784639908c8d743807

---
+++ dev/bench/data.js
@@ -0,0 +1,122 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1608784482064,
+  "repoUrl": "https://github.com/al2me6/evanescence",
+  "entries": {
+    "evanescence_core benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "alvin.q.meng@gmail.com",
+            "name": "al2me6",
+            "username": "al2me6"
+          },
+          "committer": {
+            "email": "alvin.q.meng@gmail.com",
+            "name": "al2me6",
+            "username": "al2me6"
+          },
+          "distinct": true,
+          "id": "9331c16d3ee6b5c30b8eb7af92142818259bf009",
+          "message": "fix(ci): fix benchmark",
+          "timestamp": "2020-12-23T22:16:01-05:00",
+          "tree_id": "e2078d9dea615a97861e5aa35eff4126e0729c43",
+          "url": "https://github.com/al2me6/evanescence/commit/9331c16d3ee6b5c30b8eb7af92142818259bf009"
+        },
+        "date": 1608784480103,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "factorials/factorial/0",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/1",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/2",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/3",
+            "value": 7,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/4",
+            "value": 10,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/5",
+            "value": 13,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/6",
+            "value": 13,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/7",
+            "value": 14,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/8",
+            "value": 13,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/9",
+            "value": 15,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/10",
+            "value": 16,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/11",
+            "value": 16,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/12",
+            "value": 15,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/13",
+            "value": 16,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "factorials/factorial/14",
+            "value": 16,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
