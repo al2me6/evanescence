926daeee213c3a0011c62ced8bede6107a4199cb
al2me6 <alvin.q.meng@gmail.com>
2021-01-06 14:04:43 -0500

    meta(core): disable annoying cast warnings

--- evanescence_core/src/lib.rs
+++ evanescence_core/src/lib.rs
@@ -1,11 +1,13 @@
 #![feature(destructuring_assignment)]
 #![warn(clippy::pedantic, clippy::integer_division)]
 #![allow(
-    clippy::excessive_precision,
-    clippy::cast_lossless,
-    clippy::must_use_candidate,
-    clippy::non_ascii_literal,
-    clippy::unreadable_literal
+    clippy::cast_lossless, // Stylistic.
+    clippy::cast_possible_wrap, // We generally work with smaller values, so this should not be a concern.
+    clippy::cast_precision_loss, // As with above.
+    clippy::excessive_precision, // We have many machine-generated values that are not worth fixing.
+    clippy::must_use_candidate, // Annoying.
+    clippy::non_ascii_literal, // It's 2021... Unicode support is expected.
+    clippy::unreadable_literal, // We have many machine-generated values that are not worth fixing.
 )]
 
 pub mod geometry;
