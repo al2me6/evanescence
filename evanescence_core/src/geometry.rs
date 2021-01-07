b2326a18d29ed62e21df5b4d1a0cce2652e3c363
al2me6 <alvin.q.meng@gmail.com>
2021-01-07 14:49:15 -0500

    feat(core): add types representing a plane and a grid of values

--- evanescence_core/src/geometry.rs
+++ evanescence_core/src/geometry.rs
@@ -4,6 +4,7 @@ use std::iter;
 use std::ops::{Add, AddAssign, Div, Mul, Neg, Sub};
 
 use getset::{CopyGetters, Getters};
+use strum::Display;
 
 use crate::utils::new_rng;
 
@@ -244,6 +245,36 @@ impl Point {
     }
 }
 
+/// Type representing a coordinate plane.
+#[derive(Clone, Copy, Debug, Display)]
+pub enum Plane {
+    XY,
+    YZ,
+    ZX,
+}
+
+impl Plane {
+    /// Get the basis vectors for the coordinate plane, in terms of the standard basis for `R^3`.
+    ///
+    /// The basis is oriented such that `e_1 × e_2` is the third standard basis vector for `R^3`.
+    pub fn basis_vectors(self) -> (Vec3, Vec3) {
+        match self {
+            Self::XY => (Vec3::I, Vec3::J),
+            Self::YZ => (Vec3::J, Vec3::K),
+            Self::ZX => (Vec3::K, Vec3::I),
+        }
+    }
+
+    /// Get the names of the two coordinate axes defining the plane, in order.
+    pub fn axes_names(self) -> (char, char) {
+        match self {
+            Self::XY => ('x', 'y'),
+            Self::YZ => ('y', 'z'),
+            Self::ZX => ('z', 'x'),
+        }
+    }
+}
+
 /// A point and the value of a function evaluated at that point.
 pub type PointValue<T> = (Point, T);
 
@@ -315,6 +346,107 @@ impl<T> From<Vec<PointValue<T>>> for ComponentForm<T> {
             vals.push(val);
         });
         ComponentForm { xs, ys, zs, vals }
+    }
+}
+
+/// A grid of points on a specified plane and a value associated with each point in the grid.
+///
+/// This type represents values in the manner expected by Plotly's "Surface" plot. That is,
+/// the x coordinates (here `col_coords`) and y coordinates (here `row_coords`) of the points
+/// in the grid are each represented as a one-dimensional list. The points in the grid are
+/// then taken to be the Cartesian product of the x and y coordinates.
+///
+/// Values, in turn, are stored as a two-dimensional list: a column of rows, where a "column"
+/// has constant `row_coord` value and a "row" has constant `col_coord` value.
+///
+/// Graphically, the layout is as follows:
+///
+/// ```text
+///      [   3    6    9]  <---- `col_coords`
+/// ⎴  [
+/// 2    [v_00 v_01 v_02],
+/// 4    [v_10 v_11 v_12],
+/// 6    [v_20 v_21 v_22],
+/// 8    [v_30 v_31 v_32],
+/// ⎵  ]  <---- `vals`
+/// ^---- `row_coords`
+/// ```
+///
+/// Taking this `GridValues` to be on the xy-plane, the points would thus be found at the following
+/// positions:
+///
+/// ```text
+///  y
+///  ^
+///  |
+///  8      v_30   v_31   v_32
+///  |
+///  6      v_20   v_21   v_22
+///  |
+///  4      v_10   v_11   v_12
+///  |
+///  2      v_00   v_01   v_02
+///  |
+/// -+------3------6------9------>x
+///  |
+///  ```
+///
+/// # Safety
+/// `col_coords`, `row_coords`, and `vals` must have matching shapes (see [`Self::new`]).
+#[derive(Getters)]
+#[getset(get = "pub")]
+pub struct GridValues<T> {
+    /// The plane on which the grid is situated.
+    plane: Plane,
+    /// The "horizontal" coordinate of each column.
+    col_coords: Vec<f64>,
+    /// The "vertical" coordinate of each row.
+    row_coords: Vec<f64>,
+    /// The values associated with the grid, stored as a column of rows.
+    vals: Vec<Vec<T>>,
+}
+
+impl<T> GridValues<T> {
+    /// Create a new `GridValues` from components.
+    ///
+    /// # Panics
+    /// This function will panic if the `Vec`s passed in do not have the correct shape:
+    ///
+    /// ```should_panic
+    /// # use evanescence_core::geometry::{Plane, GridValues};
+    /// GridValues::<f64>::new(
+    ///     Plane::XY,
+    ///     vec![0.0, 1.0], // There are two columns.
+    ///     vec![0.0, 1.0, 2.0], // There are three rows.
+    ///     vec![ // This is a column of rows.
+    ///         vec![3.1, 4.1],
+    ///         vec![1.5, 9.2],
+    ///         // The third row is missing!!
+    ///     ],
+    /// );
+    /// ```
+    pub fn new(
+        plane: Plane,
+        col_coords: Vec<f64>,
+        row_coords: Vec<f64>,
+        vals: Vec<Vec<T>>,
+    ) -> Self {
+        // SAFETY: Verify that the passed `Vec`s have the correct shape.
+        assert_eq!(row_coords.len(), vals.len());
+        vals.iter()
+            .for_each(|row| assert_eq!(col_coords.len(), row.len()));
+        Self {
+            plane,
+            col_coords,
+            row_coords,
+            vals,
+        }
+    }
+
+    /// Decompose `self` into a 3-tuple of column coordinates ("x coordinates"), row coordinates
+    /// ("y coordinates"), and values, in that order.
+    pub fn into_components(self) -> (Vec<f64>, Vec<f64>, Vec<Vec<T>>) {
+        (self.col_coords, self.row_coords, self.vals)
     }
 }
 
