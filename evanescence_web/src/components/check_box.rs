34eb9c73e3f4e3fbb8e14f5a60430efbe5d0fab6
al2me6 <alvin.q.meng@gmail.com>
2021-01-17 13:00:54 -0500

    feat(web): add a check box component

---
+++ evanescence_web/src/components/check_box.rs
@@ -0,0 +1,58 @@
+use web_sys::HtmlInputElement;
+use yew::{html, Callback, Component, ComponentLink, Html, NodeRef, Properties, ShouldRender};
+use yewtil::NeqAssign;
+
+pub(crate) struct CheckBox {
+    link: ComponentLink<Self>,
+    props: CheckBoxProps,
+    state: bool,
+    node_ref: NodeRef,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub(crate) struct CheckBoxProps {
+    pub(crate) id: String,
+    pub(crate) onchange: Callback<bool>,
+    pub(crate) initial_state: bool,
+    pub(crate) label: String,
+}
+
+impl Component for CheckBox {
+    type Message = ();
+    type Properties = CheckBoxProps;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let state = props.initial_state;
+        Self {
+            link,
+            props,
+            state,
+            node_ref: NodeRef::default(),
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        self.state = self.node_ref.cast::<HtmlInputElement>().unwrap().checked();
+        self.props.onchange.emit(self.state);
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props.neq_assign(props)
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+            <input
+                ref = self.node_ref.clone(),
+                type = "checkbox",
+                id = self.props.id,
+                onchange = self.link.callback(|_| ()),
+                checked = self.state
+            />
+            <label for = self.props.id>{ &self.props.label }</label>
+            </>
+        }
+    }
+}
--- evanescence_web/src/components/mod.rs
+++ evanescence_web/src/components/mod.rs
@@ -1,5 +1,7 @@
+pub(crate) mod check_box;
 pub(crate) mod dropdown;
 pub(crate) mod raw_span;
 
+pub(crate) use check_box::CheckBox;
 pub(crate) use dropdown::Dropdown;
 pub(crate) use raw_span::RawSpan;
