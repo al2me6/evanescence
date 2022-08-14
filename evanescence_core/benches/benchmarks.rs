71e4089434434e5e61b95bb8a9429dc1964e2009
Alvin Meng <alvin.q.meng@gmail.com>
2022-08-14 17:07:52 -0400

    meta(core): rewrite benchmarks

--- evanescence_core/benches/benchmarks.rs
+++ evanescence_core/benches/benchmarks.rs
@@ -1,71 +1,157 @@
+#![feature(once_cell)]
+
+use std::f32::consts::{FRAC_1_SQRT_2, SQRT_2};
+use std::iter;
+use std::sync::LazyLock;
 use std::time::Duration;
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use evanescence_core::geometry::storage::Soa;
-use evanescence_core::numerics::monte_carlo::accept_reject::AcceptReject;
-use evanescence_core::numerics::special::factorial::DoubleFactorial;
-use evanescence_core::numerics::special::orthogonal_polynomials::{
-    associated_laguerre, renormalized_associated_legendre,
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use evanescence_core::geometry::point::SphericalPoint3;
+use evanescence_core::geometry::region::{
+    BallCenteredAtOrigin, BoundingRegion, CubeCenteredAtOrigin, Region,
 };
-use evanescence_core::orbital::{Qn, Real};
+use evanescence_core::lc;
+use evanescence_core::numerics::monte_carlo::accept_reject::AcceptReject;
+use evanescence_core::numerics::polynomial::Polynomial;
+use evanescence_core::numerics::random::WyRand;
+use evanescence_core::numerics::{normalize, Function};
+use evanescence_core::orbital::hybrid::Hybrid;
+use evanescence_core::orbital::{Complex, Qn, Real};
+use nalgebra::Point3;
 
-pub fn bench_numerics(c: &mut Criterion) {
-    let mut fact_group = c.benchmark_group("factorials");
-    for n in 0_u64..=14 {
-        fact_group.bench_with_input(BenchmarkId::new("multifactorial_2", n), &n, |b, n| {
-            b.iter(|| n.double_factorial())
+fn polynomial(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("polynomial");
+    group.throughput(Throughput::Elements(1));
+
+    let rng = &mut WyRand::new_seed(0xDEAD_BEEF);
+
+    for n in 1..=10 {
+        let poly: Polynomial = iter::repeat_with(|| rng.gen_f32())
+            .take(n + 1)
+            .map(|a_i| normalize(0.0..=1.0, -10.0..=10.0, a_i))
+            .collect();
+
+        group.bench_function(format!("horner_deg{n}"), |b| {
+            b.iter(|| {
+                black_box(poly.evaluate_horner(std::f32::consts::TAU));
+            })
         });
     }
-    fact_group.finish();
 
-    let mut poly_group = c.benchmark_group("orthogonal_polynomials");
-    for params in (1..=6).flat_map(|a| (0..=a).map(move |b| (a, b))) {
-        poly_group.bench_with_input(
-            BenchmarkId::new("associated_legendre", format!("{}_{}", params.0, params.1)),
-            &params,
-            |b, lm| b.iter(|| renormalized_associated_legendre(*lm, 27.1828)),
-        );
-
-        poly_group.bench_with_input(
-            BenchmarkId::new("associated_laguerre", format!("{}_{}", params.0, params.1)),
-            &params,
-            |b, lm| {
-                let evaluator = associated_laguerre(lm.0, lm.1);
-                b.iter(|| evaluator.evaluate_horner(27.1828));
-            },
-        );
-    }
-    poly_group.finish();
+    group.finish();
 }
 
-pub fn bench_monte_carlo(c: &mut Criterion) {
-    let mut group = c.benchmark_group("monte_carlo");
-    group
-        .sample_size(10)
-        .warm_up_time(Duration::from_secs(10))
-        .measurement_time(Duration::from_secs(30));
-    group.throughput(Throughput::Elements(131_072));
-    for qn in Qn::enumerate_up_to_n(5).unwrap() {
-        group.bench_function(
-            BenchmarkId::new(
-                "real",
-                format!(
-                    "{}_{}_{}",
-                    qn.n(),
-                    qn.l(),
-                    qn.m().to_string().replace('-', "n")
-                ),
-            ),
-            |b| {
-                b.iter(|| {
-                    AcceptReject::new(Real::new(qn))
-                        .take(131_072)
-                        .collect::<Soa<3, _>>()
-                })
-            },
-        );
-    }
+fn sampling(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("sampling");
+    group.throughput(Throughput::Elements(1));
+
+    group.bench_function("ball/spherical_point_3", |b| {
+        let ball = BallCenteredAtOrigin { radius: 20. };
+        let rng = &mut WyRand::new();
+        b.iter(|| black_box(Region::<3, SphericalPoint3>::sample(&ball, rng)));
+    });
+
+    group.bench_function("ball/point_3", |b| {
+        let ball = BallCenteredAtOrigin { radius: 20. };
+        let rng = &mut WyRand::new();
+        b.iter(|| black_box(Region::<3, Point3<_>>::sample(&ball, rng)));
+    });
+
+    group.bench_function("cube/point_3", |b| {
+        let cube = CubeCenteredAtOrigin { side_length: 40. };
+        let rng = &mut WyRand::new();
+        b.iter(|| black_box(Region::<3, Point3<_>>::sample(&cube, rng)));
+    });
+
+    group.finish();
 }
 
-criterion_group!(benches, bench_numerics, bench_monte_carlo);
+static QNS: LazyLock<Vec<(usize, Qn)>> = LazyLock::new(|| {
+    Qn::enumerate_up_to_n(5)
+        .unwrap()
+        .enumerate()
+        // .filter(|(_, qn)| [1, 2, 5].contains(&qn.n()))
+        .collect()
+});
+
+fn atomic_real(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("atomic_real_evaluation");
+    group.throughput(Throughput::Elements(1));
+    for (idx, qn) in QNS.iter() {
+        group.bench_function(format!("{idx:02}_{qn}"), |b| {
+            let real = Real::new(*qn);
+            let pt = real.bounding_region().sample(&mut WyRand::new());
+            b.iter(|| black_box(real.evaluate(&pt)));
+        });
+    }
+    group.finish();
+
+    let mut group = crit.benchmark_group("atomic_real_monte_carlo_startup");
+    for (idx, qn) in QNS.iter() {
+        group.bench_function(format!("{idx:02}_{qn}"), |b| {
+            b.iter(|| black_box(AcceptReject::new(Real::new(*qn))));
+        });
+    }
+    group.finish();
+
+    let mut group = crit.benchmark_group("atomic_real_monte_carlo_sampling");
+    group.throughput(Throughput::Elements(1));
+    for (idx, qn) in QNS.iter() {
+        group.bench_function(format!("{idx:02}_{qn}"), |b| {
+            let mut sampler = AcceptReject::new(Real::new(*qn));
+            b.iter(|| black_box(sampler.next()));
+        });
+    }
+    group.finish();
+}
+
+fn atomic_complex(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("atomic_complex_monte_carlo_sampling");
+    group.throughput(Throughput::Elements(1));
+    for (idx, qn) in QNS.iter() {
+        group.bench_function(format!("{idx:02}_{qn}"), |b| {
+            let mut sampler = AcceptReject::new(Complex::new(*qn));
+            b.iter(|| black_box(sampler.next()));
+        });
+    }
+    group.finish();
+}
+
+fn hybrid(crit: &mut Criterion) {
+    let mut group = crit.benchmark_group("hybrid_monte_carlo_sampling");
+    group.throughput(Throughput::Elements(1));
+
+    group.bench_function("sp2", |b| {
+        let sp2 = lc! {
+            overall: 0.408_248_3,
+            (2, 0, 0) * SQRT_2,
+            (2, 1, 1) * 1.0,
+            (2, 1, -1) * 1.732_050_8,
+        };
+        let mut sampler = AcceptReject::new(Hybrid::new(sp2));
+        b.iter(|| black_box(sampler.next()));
+    });
+
+    group.bench_function("sp3d2", |b| {
+        let sp3d2 = lc! {
+            overall: 0.408_248_3,
+            (3, 0, 0) * 1.0,
+            (3, 1, 1) * 1.732_050_8,
+            (3, 2, 0) * -FRAC_1_SQRT_2,
+            (3, 2, 2) * 1.732_050_8 * FRAC_1_SQRT_2,
+        };
+        let mut sampler = AcceptReject::new(Hybrid::new(sp3d2));
+        b.iter(|| black_box(sampler.next()));
+    });
+
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default()
+        .warm_up_time(Duration::from_secs(2))
+        .measurement_time(Duration::from_secs(4));
+    targets = polynomial, sampling, atomic_real, atomic_complex, hybrid
+}
 criterion_main!(benches);
