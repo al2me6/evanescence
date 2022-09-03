f723136f433168f7b3738efbb1d57b0884a5d78f
Alvin Meng <alvin.q.meng@gmail.com>
2022-09-03 16:33:32 -0400

    refactor(cli): pull sampling and json writing into functions

--- evanescence_cli/src/main.rs
+++ evanescence_cli/src/main.rs
@@ -4,9 +4,12 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::{AppSettings, Parser, Subcommand};
+use evanescence_core::geometry::point::IPoint;
 use evanescence_core::geometry::region::BoundingRegion;
 use evanescence_core::geometry::storage::struct_of_arrays::IntoSoa;
+use evanescence_core::geometry::storage::Soa;
 use evanescence_core::numerics::monte_carlo::accept_reject::AcceptReject;
+use evanescence_core::numerics::monte_carlo::MonteCarlo;
 use evanescence_core::orbital::{AtomicReal, Qn};
 use serde::Serialize;
 
@@ -35,6 +38,49 @@ enum Command {
     AtomicReal { n: u32, l: u32, m: i32 },
 }
 
+fn sample_real<P: IPoint<3>, M: MonteCarlo<3, P, Output = f32>>(
+    sampler: &mut M,
+    count: usize,
+    normalize_magnitudes: bool,
+) -> Soa<3, f32> {
+    let mut samples = sampler.take(count as _).into_soa();
+    if normalize_magnitudes {
+        let max = samples
+            .values()
+            .iter()
+            .map(|p| p.abs())
+            .max_by(f32::total_cmp)
+            .unwrap();
+        for v in samples.values_mut() {
+            *v /= max;
+        }
+    }
+    samples
+}
+
+fn write_json(out: Option<&PathBuf>, json: impl Serialize) -> Result<()> {
+    let out_writer: Box<dyn Write> = if let Some(out) = out {
+        Box::new(
+            OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open(out)?,
+        )
+    } else {
+        Box::new(std::io::stdout())
+    };
+    let mut writer = BufWriter::new(out_writer);
+    let write_json = if out.is_some() {
+        serde_json::to_writer
+    } else {
+        serde_json::to_writer_pretty
+    };
+    write_json(&mut writer, &json)?;
+    writer.flush()?;
+    Ok(())
+}
+
 #[derive(Serialize)]
 struct AtomicRealJson {
     qn: Qn,
@@ -44,7 +90,6 @@ struct AtomicRealJson {
     z: Vec<f32>,
     psi: Vec<f32>,
 }
-
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
@@ -55,13 +100,8 @@ fn main() -> Result<()> {
             let bounding_sphere_radius = atomic_real.bounding_region().radius;
 
             let sampler = &mut AcceptReject::new(atomic_real);
-            let ([x, y, z], mut psi) = sampler.take(cli.count as _).into_soa_components();
-            if cli.normalize_magnitudes {
-                let max = psi.iter().map(|p| p.abs()).max_by(f32::total_cmp).unwrap();
-                for p in &mut psi {
-                    *p /= max;
-                }
-            }
+            let ([x, y, z], psi) =
+                sample_real(sampler, cli.count as _, cli.normalize_magnitudes).into_components();
 
             let json = AtomicRealJson {
                 qn,
@@ -71,26 +111,7 @@ fn main() -> Result<()> {
                 z,
                 psi,
             };
-
-            let out: Box<dyn Write> = if let Some(out) = &cli.out {
-                Box::new(
-                    OpenOptions::new()
-                        .create(true)
-                        .write(true)
-                        .truncate(true)
-                        .open(out)?,
-                )
-            } else {
-                Box::new(std::io::stdout())
-            };
-            let mut writer = BufWriter::new(out);
-            let write_json = if cli.out.is_some() {
-                serde_json::to_writer
-            } else {
-                serde_json::to_writer_pretty
-            };
-            write_json(&mut writer, &json)?;
-            writer.flush()?;
+            write_json(cli.out.as_ref(), json)?;
         }
     }
 
